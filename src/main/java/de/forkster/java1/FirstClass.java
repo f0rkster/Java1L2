9de0bbd6978cec4c98ae295b72f8c31b5080bbf7
F0rkster <matthias-gabel@hotmail.de>
2020-11-09 15:29:29 +0100

    Done!

--- src/main/java/de/forkster/java1/FirstClass.java
+++ src/main/java/de/forkster/java1/FirstClass.java
@@ -1,9 +1,44 @@
 package de.forkster.java1;
 
+import java.lang.management.ManagementFactory;
+
 class FirstClass {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+    public static void main(String[] args)
+    {
+        GameObject Obj1 = new GameObject();
+        Obj1.Spell = "Fitz";
+        Obj1.Value = 5;
+        GameObject Obj2 = new GameObject();
+        Obj2.Spell = "Quark";
+        Obj2.Value = 7;
 
+        final GameObject DEVISOR [] = {Obj1, Obj2};
+
+        String Output = "";
+
+        int index = 1;
+        while (index <= 100)
+        {
+            for (int indexOfDevisor = 0; indexOfDevisor < DEVISOR.length; indexOfDevisor++)
+            {
+                if ( index % DEVISOR[indexOfDevisor].Value == 0)
+                {
+                    Output += DEVISOR[indexOfDevisor].Spell;
+                }
+            }
+
+            if (Output != "")
+            {
+                System.out.println(Output);
+            }
+            else
+            {
+                System.out.println(index);
+            }
+
+            Output = "";
+            index++;
+        }
+    }
 }
---
+++ src/main/java/de/forkster/java1/GameObject.java
@@ -0,0 +1,7 @@
+package de.forkster.java1;
+
+public class GameObject
+{
+    public int Value;
+    public String Spell;
+}
