b9679389ac11951de16060260c5132e4e9574bce
F0rkster <matthias-gabel@hotmail.de>
2020-11-09 13:38:08 +0100

    Ready to start

---
+++ src/main/java/de/forkster/java1/FirstClass.java
@@ -0,0 +1,9 @@
+package de.forkster.java1;
+
+class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/test/java/de/forkster/java1/FirstClassTest.java
@@ -0,0 +1,12 @@
+package de.forkster.java1;
+
+class MyFirstStartClassTest {
+
+    void check_if_something_does_something() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
